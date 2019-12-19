509c5373babb479ca22e8866cda7a7f48607b4b3
Taras Venger <taras-venger@users.noreply.github.com>
2019-12-19 14:02:57 +0200

    Styled footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,23 +2,31 @@ import React from 'react';
 import { Button } from 'antd';
 import styled from 'styled-components';
 
-const Wrapper = styled.footer`
-  // position: fixed;
-  buttom: 0px;
-  padding: 10px;
+const StyledFooter = styled.footer`
+  position: fixed;
+  width: 100%;
+  left: 0px;
+  bottom: 0px;
+  padding: 0px 30px;
+  background-color: white;
+`;
+
+const StyledDiv = styled.div`
   display: flex;
   justify-content: flex-end;
-  align-items: flex-end;
-  border-top: 1px solid lightgrey;
+  padding: 20px 0px;
+  border-top: 1px solid #e5e5e5;
 `;
 
 function Footer(props) {
   return (
-    <Wrapper>
-      <Button type='primary' size='large' style={{ width: '150px' }}>
-        {props.btnLable}
-      </Button>
-    </Wrapper>
+    <StyledFooter>
+      <StyledDiv>
+        <Button type='primary' size='large' style={{ width: '150px' }}>
+          {props.btnLable}
+        </Button>
+      </StyledDiv>
+    </StyledFooter>
   );
 }
 
